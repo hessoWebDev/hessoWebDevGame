2bc7377497849326bfcd77c2ff44d8c65a2570e7
Poluux <hugoboss82001@hotmail.com>
2024-12-10 16:47:22 +0100

    Created JSON file for levels and implemented it. + added possibility of changing sprite orientation for ennemy and choosing movement direction at initialisation

--- js/enemy.js
+++ js/enemy.js
@@ -1,35 +1,65 @@
 class Enemy {
-    constructor(x, y, width, height, speed, moveDistance, type) {
-        this.startX = x; // Starting position
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.sprite = new Image();
-        this.velocity = { x: speed, y: 0 }; // Horizontal speed
-        this.moveDistance = moveDistance; // Total distance to move
-        this.travelled = 0; // Distance travelled so far
+  constructor(
+    x,
+    y,
+    width,
+    height,
+    speed,
+    moveDistance,
+    type,
+    direction = "right"
+  ) {
+    this.startX = x; // Starting position
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.sprite = new Image();
+    this.velocity = { x: direction === "left" ? -speed : speed, y: 0 }; // Horizontal speed
+    this.moveDistance = moveDistance; // Total distance to move
+    this.travelled = 0; // Distance travelled so far
 
-        if (type === "robot") {
-            this.sprite.src = './assets/sprites/robotEnemy.png';
-        } else if(type === "greenMonster") {
-            this.sprite.src = './assets/sprites/greenEnemy.png';
-        }
+    if (type === "robot") {
+      this.sprite.src = "./assets/sprites/robotEnemy.png";
+    } else if (type === "greenMonster") {
+      this.sprite.src = "./assets/sprites/greenEnemy.png";
     }
+  }
 
-    draw(ctx, camera) {
-        ctx.drawImage(this.sprite, this.x - camera.offsetX, this.y, this.width, this.height);
-    }
+  draw(ctx, camera) {
+    ctx.save();
 
-    update(deltaTime) {
-        // Move the enemy with game speed
-        this.x += this.velocity.x * gameSpeed;
-        this.travelled += Math.abs(this.velocity.x * gameSpeed);
-    
-        // Reverse direction if the enemy exceeds its movement range
-        if (this.travelled >= this.moveDistance) {
-            this.velocity.x *= -1; // Reverse direction
-            this.travelled = 0; // Reset distance travelled
-        }
+    // Check the direction the ennemy goes to change the orientation of the sprite
+    if (this.velocity.x < 0) {
+      ctx.scale(-1, 1);
+      ctx.drawImage(
+        this.sprite,
+        -(this.x - camera.offsetX + this.width),
+        this.y,
+        this.width,
+        this.height
+      );
+    } else {
+      ctx.drawImage(
+        this.sprite,
+        this.x - camera.offsetX,
+        this.y,
+        this.width,
+        this.height
+      );
     }
+    ctx.restore();
+  }
+
+  update(deltaTime) {
+    // Move the enemy with game speed
+    this.x += this.velocity.x * gameSpeed;
+    this.travelled += Math.abs(this.velocity.x * gameSpeed);
+
+    // Reverse direction if the enemy exceeds its movement range
+    if (this.travelled >= this.moveDistance) {
+      this.velocity.x *= -1; // Reverse direction
+      this.travelled = 0; // Reset distance travelled
+    }
+  }
 }
